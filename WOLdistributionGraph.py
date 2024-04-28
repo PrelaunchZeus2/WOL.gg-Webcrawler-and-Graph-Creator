83f107b12df6a8ca92d7306a7a3f0ba3e7e425c3
BuildTools <unconfigured@null.spigotmc.org>
2024-04-28 16:33:12 -0700

    Added output to .csv and gitignore to not upload a csv file every time

--- WOLdistributionGraph.py
+++ WOLdistributionGraph.py
@@ -3,17 +3,25 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import concurrent.futures
 import seaborn as sns
+import threading
+import csv
 
-sns.set_theme(context = 'talk', style='whitegrid')
+sns.set_theme(context = 'notebook', style='white', font_scale=1.5)
+sns.color_palette("tab10")
+
+#thread lock
+print_lock = threading.Lock() #lock to print which page is being processed
 
 #Url to scrape
 baseurl = 'https://wol.gg/top/world/'
 
 #Number of pages that we want to scrape
-n_pages = 200
+n_pages = 50
 
 #Function to scrape a single page
 def scrape_page(i):
+    with print_lock:
+        print(f'Processing page {i}')
     # Create URL for this page
     url = baseurl + str(i) + '/'
 
@@ -30,13 +38,20 @@ def scrape_page(i):
 
     page_data = []
     for row in rows:
+        region = row.find('span', class_='region')
         rank = row.find('span', class_='rank').text.strip().replace('.', '')
+        name = row.find('span', class_='name').text.strip()
         days = row.find('span', class_='days').text.strip().replace('.', '').rstrip('d')
-        page_data.append([int(rank), int(days)])
+        page_data.append([str(region), int(rank), str(name), int(days)])
+
+    #write data to csv file
+    with open('WOL_Top_Player_Data.csv', 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(page_data)
 
     return page_data
 
-#MULTI THREADING!!!!! BRRRRR
+#MULTI-THREADING!!!!! BRRRRR
 with concurrent.futures.ThreadPoolExecutor() as executor:
     #Use list comprehension to create a list of futures
     futures = [executor.submit(scrape_page, i) for i in range(1, n_pages + 1)]
@@ -45,7 +60,7 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     data = []
     for future in concurrent.futures.as_completed(futures):
         data.extend(future.result())
-
+#I dont really know how to use multi threading but according to stack overflow and other *cough cough ai assisted* research this should work, I should probably learn about it more
 
 #Create graph from data
 x_values = [row[1] for row in data]
